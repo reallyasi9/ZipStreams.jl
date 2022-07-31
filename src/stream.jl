9c5a7bc1e201df7bc042a2bcbe6d899d5945f315
Phil Killewald <reallyasi9@users.noreply.github.com>
2022-07-31 01:39:32 -0400

    Fix SentinelInputStream

--- src/stream.jl
+++ src/stream.jl
@@ -1,4 +1,6 @@
-import Base: bytesavailable, close, eof, isopen, unsafe_read, unsafe_write
+using BufferedStreams
+
+import Base: bytesavailable, close, eof, isopen, unsafe_read, read
 
 const CRC32_INIT = 0x00000000
 
@@ -6,63 +8,180 @@ function crc32(data::Ptr{UInt8}, n::UInt, crc::UInt32=CRC32_INIT)
     return ccall((:crc32, "libz"), Culong, (Culong, Ptr{Cchar}, Cuint), crc, data, n) % UInt32
 end
 
-crc32(data::Vector{UInt8}, crc::UInt32) = GC.@preserve data crc32(pointer(data), UInt(length(data)), crc)
-crc32(s::String, crc::UInt32) = crc32(Vector{UInt8}(s), crc)
+crc32(data::Vector{UInt8}, crc::UInt32=CRC32_INIT) = GC.@preserve data crc32(pointer(data), UInt(length(data)), crc)
+crc32(s::String, crc::UInt32=CRC32_INIT) = crc32(Vector{UInt8}(s), crc)
 
 """
-    CRC32Stream
+    CRC32InputStream
 
-An IOStream that calculates a CRC-32 checksum for read and written data.
+A wrapper around an IO object that that calculates a CRC-32 checksum for data
+read.
 """
-mutable struct CRC32Stream <: IO
-    _io::IO
-    crc32_read::UInt32
-    crc32_write::UInt32
-
-    CRC32Stream(io::IO) = new(io, CRC32_INIT, CRC32_INIT)
+mutable struct CRC32InputStream{T} <: IO
+    source::T
+    bytes_read::UInt64
+    crc32::UInt32
 end
 
-Base.bytesavailable(s::CRC32Stream) = bytesavailable(s._io)
-Base.close(s::CRC32Stream) = close(s._io)
-Base.eof(s::CRC32Stream) = eof(s._io)
-Base.isopen(s::CRC32Stream) = isopen(s._io)
+function CRC32InputStream(source::T) where {T}
+    return CRC32InputStream{T}(source, 0, CRC32_INIT)
+end
 
-function Base.unsafe_read(s::CRC32Stream, p::Ptr{UInt8}, nb::UInt)
-    unsafe_read(s._io, p, nb)
-    s.crc32_read = crc32(p, nb, s.crc32_read)
+Base.bytesavailable(s::CRC32InputStream) = bytesavailable(s.source)
+Base.close(s::CRC32InputStream) = close(s.source)
+Base.eof(s::CRC32InputStream) = eof(s.source)
+Base.isopen(s::CRC32InputStream) = isopen(s.source)
+
+function Base.unsafe_read(s::CRC32InputStream, p::Ptr{UInt8}, nb::UInt)
+    unsafe_read(s.source, p, nb)
+    # NOTE: might overflow
+    s.bytes_read += nb
+    s.crc32 = crc32(p, nb, s.crc32)
     return
 end
 
-function Base.unsafe_write(s::CRC32Stream, p::Ptr{UInt8}, nb::UInt)
-    unsafe_write(s._io, p, nb)
-    s.crc32_write = crc32(p, nb, s.crc32_write)
-    return
+function Base.read(s::CRC32InputStream, ::Type{UInt8})
+    x = read(s.source, UInt8)
+    s.bytes_read += 1
+    s.crc32 = crc32([x], s.crc32)
+    return x
 end
 
 """
-    TruncatedStream
+    TruncatedInputStream
 
-An IOStream that stops after reading a certain number of bytes.
+A wrapper around an IO object that emulates a fixed-size array without actually
+filling and using a fixed-size array.
 """
-mutable struct TruncatedStream <: IO
-    _io::IO
-    nb::UInt64
-
-    TruncatedStream(io::IO, nb::UInt64=typemax(UInt64)) = new(io, nb)
+mutable struct TruncatedInputStream{T} <: IO
+    source::T
+    max_bytes::UInt64
+    bytes_remaining::UInt64
 end
 
-Base.bytesavailable(s::TruncatedStream) = min(bytesavailable(s._io), s.nb)
-Base.close(s::TruncatedStream) = close(s._io)
-Base.eof(s::TruncatedStream) = s.nb == 0 || eof(s._io)
-Base.isopen(s::TruncatedStream) = isopen(s._io)
+function TruncatedInputStream(source::T, nb::UInt64=typemax(UInt64)) where T
+    return TruncatedInputStream{T}(source, nb, nb)
+end
+TruncatedInputStream(source::T, nb::Union{Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64}) where T = TruncatedInputStream(source, UInt64(nb))
 
-function Base.unsafe_read(s::TruncatedStream, p::Ptr{UInt8}, nb::UInt)
-    unsafe_read(s._io, p, nb)
-    # NOTE: unsafe means unsafe!
-    s.nb -= nb
+Base.bytesavailable(s::TruncatedInputStream) = min(bytesavailable(s.source), s.bytes_remaining)
+Base.close(s::TruncatedInputStream) = close(s.source)
+Base.eof(s::TruncatedInputStream) = s.bytes_remaining == 0 || eof(s.source)
+Base.isopen(s::TruncatedInputStream) = isopen(s.source)
+
+function Base.unsafe_read(s::TruncatedInputStream, p::Ptr{UInt8}, nb::UInt)
+    nread = min(nb, bytesavailable(s))
+    unsafe_read(s.source, p, nread)
+    if nread > s.bytes_remaining
+        throw(BoundsError(s))
+    end
+    s.bytes_remaining -= nread
     return
 end
-Base.unsafe_write(s::TruncatedStream, p::Ptr{UInt8}, nb::UInt) = unsafe_write(s._io, p, nb)
+
+function Base.read(s::TruncatedInputStream, ::Type{UInt8})
+    if eof(s)
+        throw(EOFError())
+    end
+    s.bytes_remaining -= 1
+    return read(s.source, UInt8)
+end
+
+"""
+    SentinelInputStream
+
+A wrapper around an IO object that treats a sentinel as EOF.
+"""
+mutable struct SentinelInputStream{S} <: IO
+    source::BufferedInputStream{S}
+    sentinel::Vector{UInt8}
+    eof::Bool
+end
+
+function SentinelInputStream(source::S, sentinel::Vector{UInt8}) where S
+    buffer = BufferedInputStream(source)
+    mark(buffer)
+    eof = false
+    if read(buffer, length(sentinel)) == sentinel
+        eof = true
+    end
+    reset(buffer)
+    return SentinelInputStream{S}(buffer, copy(sentinel), eof)
+end
+
+function SentinelInputStream(source::S, sentinel) where S
+    buf = IOBuffer()
+    write(buf, sentinel)
+    return SentinelInputStream{S}(BufferedInputStream(source), take!(buf), false)
+end
+
+# slightly more efficient
+function SentinelInputStream(source::S, sentinel::AbstractString) where S
+    return SentinelInputStream{S}(BufferedInputStream(source), collect(codeunits(sentinel)), false)
+end
+
+Base.bytesavailable(s::SentinelInputStream) = bytesavailable(s.source)
+Base.close(s::SentinelInputStream) = close(s.source)
+Base.eof(s::SentinelInputStream) = s.eof || eof(s.source)
+Base.isopen(s::SentinelInputStream) = isopen(s.source)
+
+function Base.unsafe_read(s::SentinelInputStream, p::Ptr{UInt8}, nb::UInt)
+    if eof(s)
+        throw(EOFError())
+    end
+    # read one byte at a time, pausing if a sentinel match is found
+    j = one(UInt)
+    for i in 1:nb
+        byte = read(s.source, UInt8)
+        if eof(s.source)
+            s.eof = true
+            throw(EOFError())
+        end
+        if byte == s.sentinel[j]
+            if j == length(s.sentinel)
+                # EOF, seek backward and return
+                upanchor!(s.source)
+                skip(s.source, -length(s.sentinel))
+                s.eof = true
+                return
+            else
+                j += 1
+                # continue to pause read
+                if !isanchored(s.source)
+                    anchor!(s.source)
+                end
+                continue
+            end
+        else
+            if isanchored(s.source)
+                # TODO: read anchored data
+                chunk = takeanchored!(s.source)
+                GC.@preserve chunk unsafe_copyto!(p + i - 1, pointer(chunk), length(chunk))
+            end
+            unsafe_store!(p, byte, i)
+            j = 1
+        end
+    end
+
+    return
+end
+
+function Base.read(s::SentinelInputStream, ::Type{UInt8})
+    if eof(s)
+        throw(EOFError())
+    end
+    # Invariant: the first byte is free and not a part of the sentinel
+
+    byte = read(s.source, UInt8)
+
+    # Check if the remaining is the sentinel
+    mark(s.source)
+    if read(s.source, length(s.sentinel)) == s.sentinel
+        s.eof = true
+    end
+    reset(s.source)
+    return byte
+end
 
 """
     ZipStream
--- test/crc32stream_test.jl
+++ test/crc32stream_test.jl
@@ -1,3 +1,82 @@
 using Test
 using ZipFiles
 
+@testset "CRC32" begin
+    @testset "Array" begin
+        @test ZipFiles.crc32(UInt8[]) == 0x00000000
+        @test ZipFiles.crc32(UInt8[0]) == 0xd202ef8d
+        @test ZipFiles.crc32(UInt8[1, 2, 3, 4]) == 0xb63cfbcd
+
+        @test ZipFiles.crc32(UInt8[5, 6, 7, 8], ZipFiles.crc32(UInt8[1, 2, 3, 4])) == ZipFiles.crc32(UInt8[1, 2, 3, 4, 5, 6, 7, 8]) == 0x3fca88c5
+    end
+    @testset "String" begin
+        @test ZipFiles.crc32("") == 0x00000000
+        @test ZipFiles.crc32("The quick brown fox jumps over the lazy dog") == 0x414fa339
+
+        @test ZipFiles.crc32("Julia!", ZipFiles.crc32("Hello ")) == ZipFiles.crc32("Hello Julia!") == 0x424b94c7
+    end
+end
+
+@testset "CRC32InputStream" begin
+    s = ZipFiles.CRC32InputStream(IOBuffer(UInt8[0]))
+    @test s.crc32 == 0x00000000
+    @test s.bytes_read == 0
+    @test !eof(s)
+    @test bytesavailable(s) == 1
+
+    read(s)
+    @test s.crc32 == 0xd202ef8d
+    @test s.bytes_read == 1
+    @test eof(s)
+    @test bytesavailable(s) == 0
+    
+    @test isempty(read(s))
+
+    s = ZipFiles.CRC32InputStream(IOBuffer(UInt8[1, 2, 3, 4, 5, 6, 7, 8]))
+    @test read(s, 4) == UInt8[1, 2, 3, 4]
+    @test s.crc32 == 0xb63cfbcd
+    @test s.bytes_read == 4
+    @test !eof(s)
+    @test bytesavailable(s) == 4
+
+    @test read(s, 4) == UInt8[5, 6, 7, 8]
+    @test s.crc32 == 0x3fca88c5
+    @test s.bytes_read == 8
+    @test eof(s)
+    @test bytesavailable(s) == 0
+end
+
+@testset "TruncatedInputStream" begin
+    s = ZipFiles.TruncatedInputStream(IOBuffer("The quick brown fox jumps over the lazy dog"), 15)
+    @test !eof(s)
+    @test bytesavailable(s) == 15
+
+    @test read(s, String) == "The quick brown"
+    @test eof(s)
+    @test bytesavailable(s) == 0
+    @test read(s) == UInt8[]
+
+    s = ZipFiles.TruncatedInputStream(IOBuffer("The quick brown fox jumps over the lazy dog"), 100)
+    @test bytesavailable(s) == 43
+
+    @test read(s, String) == "The quick brown fox jumps over the lazy dog"
+    @test eof(s)
+    @test bytesavailable(s) == 0
+    @test read(s) == UInt8[]
+end
+
+@testset "SentinelInputStream" begin
+    s = ZipFiles.SentinelInputStream(IOBuffer(UInt8[0, 1, 2, 3, 4, 5, 6, 7, 8]), UInt8[4, 5, 6])
+    @test !eof(s)
+    @test_broken bytesavailable(s) == 4
+    @test read(s, UInt8) == 0x00
+    @test read(s) == UInt8[1, 2, 3]
+    @test eof(s)
+    @test read(s) == UInt8[]
+    @test_throws EOFError read(s, UInt8)
+
+    s = ZipFiles.SentinelInputStream(IOBuffer("The quick brown fox jumps over the lazy dog"), " fox")
+    @test_broken bytesavailable(s) == 15
+    @test read(s, String) == "The quick brown"
+    @test eof(s)
+end
