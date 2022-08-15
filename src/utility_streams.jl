b2c2f5c8a9d0c159043b5b43835fc663b5c577c7
Phil Killewald <reallyasi9@users.noreply.github.com>
2022-08-14 22:17:37 -0400

    Simplify streaming

--- src/utility_streams.jl
+++ src/utility_streams.jl
@@ -1,5 +1,3 @@
-using BufferedStreams
-
 import Base: bytesavailable, close, eof, isopen, unsafe_read, read
 
 """
@@ -35,42 +33,9 @@ function Base.read(s::CRC32InputStream, ::Type{UInt8})
 end
 
 """
-    CRC32OutputStream
-
-A wrapper around an IO object that that calculates a CRC-32 checksum for data
-written.
-"""
-mutable struct CRC32OutputStream{T} <: IO
-    sink::T
-    crc32::UInt32
-end
-
-function CRC32OutputStream(sink::T) where {T}
-    return CRC32OutputStream{T}(sink, 0, CRC32_INIT)
-end
-
-Base.close(s::CRC32OutputStream) = close(s.sink)
-Base.eof(s::CRC32OutputStream) = eof(s.sink)
-Base.isopen(s::CRC32OutputStream) = isopen(s.sink)
-
-function Base.unsafe_write(s::CRC32OutputStream, p::Ptr{UInt8}, nb::UInt)
-    nw = unsafe_write(s.sink, p, nb)
-    # NOTE: might overflow
-    s.crc32 = crc32(p, nw, s.crc32)
-    return nw
-end
-
-function Base.write(s::CRC32OutputStream, x::UInt8)
-    n = write(s.sink, x)
-    s.crc32 = crc32([x], s.crc32)
-    return n
-end
-
-"""
     TruncatedInputStream
 
-A wrapper around an IO object that emulates a fixed-size array without actually
-filling and using a fixed-size array.
+A wrapper around an IO object that reads up to some fixed number of bytes.
 """
 mutable struct TruncatedInputStream{T} <: IO
     source::T
@@ -88,116 +53,11 @@ Base.close(s::TruncatedInputStream) = close(s.source)
 Base.eof(s::TruncatedInputStream) = s.bytes_remaining == 0 || eof(s.source)
 Base.isopen(s::TruncatedInputStream) = isopen(s.source)
 
-function Base.unsafe_read(s::TruncatedInputStream, p::Ptr{UInt8}, nb::UInt)
-    nread = min(nb, bytesavailable(s))
-    unsafe_read(s.source, p, nread)
-    if nread > s.bytes_remaining
-        throw(BoundsError(s))
-    end
-    s.bytes_remaining -= nread
-    return
-end
-
 function Base.read(s::TruncatedInputStream, ::Type{UInt8})
     if eof(s)
         throw(EOFError())
     end
     s.bytes_remaining -= 1
     return read(s.source, UInt8)
-end
-
-"""
-    SentinelInputStream
-
-A wrapper around an IO object that treats a sentinel as EOF.
-"""
-mutable struct SentinelInputStream{S} <: IO
-    source::BufferedInputStream{S}
-    sentinel::Vector{UInt8}
-    eof::Bool
-end
-
-function SentinelInputStream(source::S, sentinel::Vector{UInt8}) where S
-    buffer = BufferedInputStream(source)
-    mark(buffer)
-    eof = false
-    if read(buffer, length(sentinel)) == sentinel
-        eof = true
-    end
-    reset(buffer)
-    return SentinelInputStream{S}(buffer, copy(sentinel), eof)
-end
-
-function SentinelInputStream(source::S, sentinel) where S
-    buf = IOBuffer()
-    write(buf, sentinel)
-    return SentinelInputStream{S}(BufferedInputStream(source), take!(buf), false)
-end
-
-# slightly more efficient
-function SentinelInputStream(source::S, sentinel::AbstractString) where S
-    return SentinelInputStream{S}(BufferedInputStream(source), collect(codeunits(sentinel)), false)
-end
-
-Base.bytesavailable(s::SentinelInputStream) = bytesavailable(s.source)
-Base.close(s::SentinelInputStream) = close(s.source)
-Base.eof(s::SentinelInputStream) = s.eof || eof(s.source)
-Base.isopen(s::SentinelInputStream) = isopen(s.source)
-
-function Base.unsafe_read(s::SentinelInputStream, p::Ptr{UInt8}, nb::UInt)
-    if eof(s)
-        throw(EOFError())
-    end
-    # read one byte at a time, pausing if a sentinel match is found
-    j = one(UInt)
-    for i in 1:nb
-        byte = read(s.source, UInt8)
-        if eof(s.source)
-            s.eof = true
-            throw(EOFError())
-        end
-        if byte == s.sentinel[j]
-            if j == length(s.sentinel)
-                # EOF, seek backward and return
-                upanchor!(s.source)
-                skip(s.source, -length(s.sentinel))
-                s.eof = true
-                return
-            else
-                j += 1
-                # continue to pause read
-                if !isanchored(s.source)
-                    anchor!(s.source)
-                end
-                continue
-            end
-        else
-            if isanchored(s.source)
-                chunk = takeanchored!(s.source)
-                GC.@preserve chunk unsafe_copyto!(p + i - 1, pointer(chunk), length(chunk))
-            end
-            unsafe_store!(p, byte, i)
-            j = 1
-        end
-    end
-
-    return
-end
-
-function Base.read(s::SentinelInputStream, ::Type{UInt8})
-    if eof(s)
-        throw(EOFError())
-    end
-    # Invariant: the first byte is free and not a part of the sentinel
-
-    byte = read(s.source, UInt8)
-
-    # Check if the remaining is the sentinel
-    mark(s.source)
-    if read(s.source, length(s.sentinel)) == s.sentinel
-        s.eof = true
-    end
-    reset(s.source)
-    return byte
 end
 
