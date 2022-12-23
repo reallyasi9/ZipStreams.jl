0e388ef612fb825ffef1966bf3bb0dd22c546f91
Phil Killewald <reallyasi9@users.noreply.github.com>
2022-12-22 23:10:19 -0500

    Add check for pull_request in push preview event

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,7 @@ using ZipStreams
 
 function should_push_preview(event_path = get(ENV, "GITHUB_EVENT_PATH", nothing))
     event_path === nothing && return false
+    "pull_request" in event || return false
     event = JSON.parsefile(event_path)
     labels = [x["name"] for x in event["pull_request"]["labels"]]
     return "push_preview" in labels
