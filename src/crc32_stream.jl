7552b27ce2fe0cc291243958fc8bc191c4ec9d85
Phil Killewald <reallyasi9@users.noreply.github.com>
2023-01-11 14:55:09 -0500

    Emulate TranscodingStream.stats

--- src/crc32_stream.jl
+++ src/crc32_stream.jl
@@ -1,3 +1,5 @@
+using TranscodingStreams
+
 """
     CRC32Sink{<:IO}
     CRC32Source{<:IO}
@@ -7,56 +9,59 @@ or out of it.
 """
 mutable struct CRC32Sink{S<:IO} <: IO
     crc32::UInt32
-    bytes_in::Int
-    bytes_out::Int
+    bytes_seen::Int
     stream::S
 end
-CRC32Sink(io::IO) = CRC32Sink(CRC32_INIT, 0, 0, io)
+CRC32Sink(io::IO) = CRC32Sink(CRC32_INIT, 0, io)
 
 mutable struct CRC32Source{S<:IO} <: IO
     crc32::UInt32
-    bytes_in::Int
-    bytes_out::Int
+    bytes_seen::Int
     stream::S
 end
-CRC32Source(io::IO) = CRC32Source(CRC32_INIT, 0, 0, io)
+CRC32Source(io::IO) = CRC32Source(CRC32_INIT, 0, io)
 
 # optimized for CRC32.jl
 function Base.write(s::CRC32Sink, a::ByteArray)
     s.crc32 = crc32(a, s.crc32)
-    s.bytes_in += sizeof(a)
     bytes_out = write(s.stream, a)
-    s.bytes_out += bytes_out
+    s.bytes_seen += bytes_out
     return bytes_out
 end
 
 # fallbacks
 function Base.unsafe_write(s::CRC32Sink, p::Ptr{UInt8}, n::UInt)
     s.crc32 = unsafe_crc32(p, n, s.crc32)
-    s.bytes_in += n
     bytes_out = unsafe_write(s.stream, p, n)
-    s.bytes_out += bytes_out
+    s.bytes_seen += bytes_out
     return bytes_out
 end
 
 function Base.unsafe_read(s::CRC32Source, p::Ptr{UInt8}, n::UInt)
-    p0 = position(s.stream)
     unsafe_read(s.stream, p, n)
-    δp = position(s.stream) - p0
     s.crc32 = unsafe_crc32(p, n, s.crc32)
-    s.bytes_in += δp
-    s.bytes_out += n
+    s.bytes_seen += n
     return nothing
 end
 
 function Base.readbytes!(s::CRC32Source, a::AbstractVector{UInt8}, nb::Integer=length(a))
-    p0 = position(s.stream)
     n = readbytes!(s.stream, a, nb) % UInt64
-    δp = position(s.stream) - p0
     s.crc32 = @GC.preserve a unsafe_crc32(pointer(a), n, s.crc32)
-    s.bytes_in += δp
-    s.bytes_out += n
+    s.bytes_seen += n
     return n
+end
+
+# emulate TranscodingStreams.stats
+for typ = (:CRC32Source, :CRC32Sink)
+    # default to using the number of bytes seen
+    @eval function stats(s::$typ)
+        return TranscodingStreams.Stats(s.bytes_seen, s.bytes_seen, s.bytes_seen, s.bytes_seen)
+    end
+
+    # actually use the stats from the stream if we are using a TranscodingStream
+    @eval function stats(s::$typ{S}) where {S <: TranscodingStream}
+        return TranscodingStreams.stats(s.stream)
+    end
 end
 
 # other IO stuff
@@ -68,8 +73,9 @@ for typ = (:CRC32Source, :CRC32Sink)
     @eval Base.seek(::$typ) = error("$typ cannot seek")
 
     @eval crc32(s::$typ) = s.crc32
-    @eval bytes_in(s::$typ) = s.bytes_in
-    @eval bytes_out(s::$typ) = s.bytes_out
+    @eval bytes_seen(s::$typ) = s.bytes_seen
+    @eval bytes_in(s::$typ) = stats(s).in
+    @eval bytes_out(s::$typ) = stats(s).out
 end
 
 Base.flush(s::CRC32Sink) = flush(s.stream)
