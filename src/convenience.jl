2ae9a2d7a861eadc80731aa2a16245ded2a40c1e
Phil Killewald <reallyasi9@users.noreply.github.com>
2023-01-26 16:31:17 -0500

    Simplify utility functions

--- src/convenience.jl
+++ src/convenience.jl
@@ -39,16 +39,7 @@ function zip_files(archive_filename::AbstractString, input_filenames::AbstractVe
     return
 end
 
-function recurse_all_files(path::AbstractString)
-    files = String[]
-    for (root, dirs, files) in walkdir(path)
-        files = joinpath.(Ref(root), files)
-        for dir in dirs
-            append!(files, recurse_all_files(joinpath(root, dir)))
-        end
-    end
-    return files
-end
+recurse_all_files(path::AbstractString) = mapreduce(((root, dirs, files),) -> joinpath.(Ref(root), files), vcat, walkdir(path))
 
 function zip_files(archive_filename::AbstractString, input_filename::AbstractString; recurse_directories::Bool=false, kwargs...)
     if isdir(input_filename)
@@ -67,12 +58,9 @@ zip_file(archive_filename::AbstractString, input_filename::AbstractString; kwarg
 
 function strip_dots(path::AbstractString)
     first_non_dot_idx = 1
-    dirs = split(path, ZIP_PATH_DELIMITER)
-    for (i, dir) in enumerate(dirs)
-        if dir != "." && dir != ".."
-            first_non_dot_idx = i
-        end
-    end
+    # This takes filesystem paths and not ZIP archive paths, so use splitpath
+    dirs = splitpath(path)
+    first_non_dot_idx = findfirst(dir -> dir != "." && dir != "..", dirs)
     return join(dirs[first_non_dot_idx:end], ZIP_PATH_DELIMITER)
 end
 
